0cd5a96447ba2d6b2d05c6d856a12157e1c53bc8
xie332752 <249420000@qq.com>
2019-12-12 16:09:33 +0800

    '初始化'

---
+++ spring-order/src/main/java/com/shsxt/RegistryApplication.java
@@ -0,0 +1,16 @@
+package com.shsxt;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer // 启动eureka注册中心
+public class RegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+
+}
